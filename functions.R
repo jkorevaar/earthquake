922132b32465cb81479a656946f02e98c04990f6
Jan Korevaar <jn.korevaar@gmail.com>
2020-10-23 18:35:21 +0300

    added geom functions

--- functions.R
+++ functions.R
@@ -2,11 +2,12 @@
 # Course - Mastering Software Development in R Capstone
 # Last Edited 20/10/2020
 
+xxx <- eq_location_clean(eq_clean_data("earthquakes-2020-10-19_15-21-05_+0300.tsv"))
+
+### eq_clean_data --------------------------------------------------------------
 
 #' eq_clean_data
-#'
 #' loads earthquake data.
-#'
 #' @param filename Name of the file containing earthquake data
 #' @return Returns earthquake data with the following cleaning -a- added 01 for
 #' empty months and days -b- removed dates in bc -c- created a DATE column with
@@ -46,20 +47,16 @@ eq_clean_data <- function(filename){
   return(data)
 }
 
-
-
+### eq_location_clean ----------------------------------------------------------
 
 #' eq_location_clean
-#'
 #' cleans location column
-#'
 #' @param data the data that is returned from eq_clean_data
 #' @return returns a data file where location is -a- trimmed of ws -b- converted
 #' to title case -c- name of country removed
 #' @importFrom stringr str_to_title
 #' @examples
 #' eq_location_clean(eq_clean_data("earthquakes-2020-10-19_15-21-05_+0300.tsv"))
-
 
 eq_location_clean <- function(data){
   # remove everything before colon
@@ -70,25 +67,50 @@ eq_location_clean <- function(data){
   return(data)
 }
 
+### geom_timeline --------------------------------------------------------------
 
 
+geom_timeline<- function(mapping = NULL, data = NULL, stat = "identity",
+                         position = "identity", na.rm = FALSE,
+                         show.legend = NA, inherit.aes = TRUE, ...) {
+  ggplot2::layer(
+    geom = GeomTimeline, mapping = mapping,
+    data = data, stat = stat, position = position,
+    show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
 
+GeomTimeline <- ggplot2::ggproto("GeomTimeline", ggplot2::Geom,
+                                 required_aes = c("x"),
+                                 default_aes = ggplot2::aes(y = 0, colour = "grey", size = 1,
+                                                            alpha = 0.5, shape = 20, fill = NA, stroke = 1),
+                                 draw_key = ggplot2::draw_key_point,
+                                 draw_panel = function(data, panel_scales, coord) {
 
+                                   coords <- coord$transform(data, panel_scales)
+                                   grid::gList(
+                                     grid::pointsGrob(x = coords$x, y= coords$y,
+                                                      pch = coords$shape,
+                                                      gp = grid::gpar(col = alpha(coords$colour, coords$alpha),
+                                                                      fill = alpha(coords$fill, coords$alpha),
+                                                                      fontsize = coords$size * .pt + coords$stroke * .stroke / 2,
+                                                                      lwd = coords$stroke * .stroke / 2)
+                                     ),
+                                     grid::segmentsGrob(x0=min(coords$x), y0= coords$y, x1 = max(coords$x), y1 = coords$y,
+                                                        gp = grid::gpar(col = "blue", lwd = 1))
+                                   )
+                                 }
+)
 
+library(ggplot2)
+library(dplyr)
+xxx <- eq_location_clean(eq_clean_data("earthquakes-2020-10-19_15-21-05_+0300.tsv"))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+xxx %>%
+dplyr::filter(lubridate::year(DATE) >= 2019 & !is.na(Total.Deaths)) %>%
+ggplot(aes(x=DATE,color=Total.Deaths, size = Total.Deaths)) +
+geom_timeline(alpha=.5)
 
 
 
